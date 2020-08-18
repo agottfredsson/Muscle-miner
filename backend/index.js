c2e1cd7d54411b80eddfb49fd9a911b4e07f0d51
Marcus Klaesson <marcusklaesson91@gmail.com>
2020-08-18 10:18:30 +0200

    test

--- backend/index.js
+++ backend/index.js
@@ -13,52 +13,53 @@ const expressWs = require("express-ws")(app);
 let database;
 
 webSocketServer.on("connection", (webSocket) => {
-  console.log("Client connected");
+    console.log("Client connected");
 
-  setInterval(() => {
-    webSocket.send("Hello World!");
-  }, 1000);
+    setInterval(() => {
+        webSocket.send("Hello World!");
+    }, 1000);
 });
 
 sqlite
-  .open({ driver: sqlite3.Database, filename: "muscle-miner.sqlite" })
-  .then((database_) => {
-    database = database_;
-  });
+    .open({ driver: sqlite3.Database, filename: "muscle-miner.sqlite" })
+    .then((database_) => {
+        database = database_;
+    });
 
 app.get("/", (request, response) => {
-  database.all("SELECT * FROM users").then((users) => {
-    response.send(users);
-  });
-});
-
-app.post("/:userName", (request, response) => {
-  database
-    .run("INSERT INTO users (name, score) VALUES (?, ?)", [
-      request.params.userName,
-      0,
-    ])
-    .then((users) => {
-      response.send(users);
+    database.all("SELECT * FROM users").then((users) => {
+        response.send(users);
     });
 });
 
+app.post("/:userName", (request, response) => {
+    database
+        .run("INSERT INTO users (name, score) VALUES (?, ?)", [
+            request.params.userName,
+            0,
+        ])
+        .then((users) => {
+            response.send(users);
+        });
+});
+
 app.use(function (req, res, next) {
-  console.log("middleware");
-  req.testing = "testing";
-  return next();
+    console.log("middleware");
+    req.testing = "testing";
+    return next();
 });
 
 app.get("/", function (req, res, next) {
-  console.log("get route", req.testing);
-  res.end();
+    console.log("get route", req.testing);
+    res.end();
 });
 
 app.ws("/", function (ws, req) {
-  ws.on("message", function (msg) {
-    console.log(msg);
-  });
-  console.log("socket", req.testing);
+    ws.on("message", function (msg) {
+        console.log(msg);
+    });
+    console.log("socket", req.testing);
+
 });
 
 app.listen(3000);
